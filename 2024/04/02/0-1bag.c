036ddb1419f0e63c56a13771ad47ae089c10fa83
hk416hasu <vbcgfh487882183@126.com>
2024-04-10 20:36:03 +0800

    update

--- 2024/04/02/0-1bag.c
+++ 2024/04/02/0-1bag.c
@@ -1,36 +1,77 @@
+/*
+    0-1背包问题
+    dp[i][j]含义：i为背包容量，j为物品个数(A1 - Aj), dp[i][j]就是在“容量为i的背包”中“装下A1至Aj这些个物品”的最大价值；
+    最优子结构是：对于第j个物品，装？or不装？谁更大？max{dp[i-wj][j-1] + pj, dp[i][j-1]};
+                装：容量减小，价值增加，更新dp值  ；不装：容量与价值均不变，dp值等于装到第j-1个物品
+*/
 #include <stdio.h>
 #include <math.h>
 #include <mem.h>
 
+void Input(int *arr, int len) {
+    arr[0] = 0;
+    for (int i = 1; i < len; i++) {
+        scanf("%d", &arr[i]);
+    }
+}
+
 int main() {
     int num = 5, capacity = 10;
-    num = num + 1;
-    capacity = capacity + 1;
-    int weight[num] = {0,3,2,4,1,6};
-    int price[num] = {0,10,7,12,4,20};
-
-    int dp[capacity][num];
-    memset(dp, 0, capacity * num * sizeof(int));
+    int n_len = num + 1;
+    int c_len = capacity + 1;
+    int weight[n_len] = {0,3,2,4,1,6};
+    int price[n_len] = {0,10,7,12,4,20};
+    int dp[c_len][n_len], trace[c_len][n_len], select[n_len] = {0};
+    memset(dp, 0, c_len * n_len * sizeof(int));
+    memset(trace, 0, c_len * n_len * sizeof(int));
 
     int w = 0, p = 0;
-    for (int i = 1; i < capacity; i++) {
-        for (int j = 1; j < num; j++) {
+    for (int i = 1; i < c_len; i++) {
+        for (int j = 1; j < n_len; j++) {
             w = weight[j];
             p = price[j];
             if (w > i) {
                 dp[i][j] = dp[i][j-1];
+                trace[i][j] = 0;
             } else {
-                dp[i][j] = fmax(dp[i-w][j-1] + p, dp[i][j-1]);
+                if (dp[i-w][j-1] + p > dp[i][j-1]) {
+                    dp[i][j] = dp[i-w][j-1] + p;
+                    trace[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i][j-1];
+                    trace[i][j] = 0;
+                }
             }
         }
     }
-
-    for (int i = 0; i < capacity; i++) {
-        for (int j = 0; j < num; j++) {
+    printf("dp:\n");
+    for (int i = 0; i < c_len; i++) {
+        for (int j = 0; j < n_len; j++) {
             printf("%d ", dp[i][j]);
         }
         printf("\n");
     }
+    printf("\ntrace:\n");
+    for (int i = 0; i < c_len; i++) {
+        for (int j = 0; j < n_len; j++) {
+            printf("%d ", trace[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    for (int i = capacity, j = num; j > 0; j--) {
+        if (trace[i][j] == 1) {
+            select[j] = 1;
+            i -= weight[j];
+        }
+    }
+    for (int i = 1; i < n_len; i++) {
+        if (select[i] == 1) {
+            printf("A%d ", i);
+        }
+    }
+    printf("\n");
 
     getchar();
     return 0;
