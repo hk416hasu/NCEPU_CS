db6d91f3b759a06f8d7825eb9fe071cb1b199171
hk416hasu <vbcgfh487882183@126.com>
2024-04-11 17:58:31 +0800

    update 递推公式

--- 2024/04/02/0-1bag.c
+++ 2024/04/02/0-1bag.c
@@ -4,6 +4,12 @@
     最优子结构是：对于第j个物品，装？or不装？谁更大？max{dp[i-wj][j-1] + pj, dp[i][j-1]};
                 装：容量减小，价值增加，更新dp值  ；不装：容量与价值均不变，dp值等于装到第j-1个物品
     遍历顺序：行从小到大，列从小到大；（由我找到的最优子结构 + 递推公式决定的）
+    递推公式：
+                        dp[i][j-1], i < wj 时
+            dp[i][j] = 
+                        max{dp[i-wj][j-1]+pj, dp[i][j-1]}, i >= wj 时
+
+
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,8 +35,9 @@ int main() {
     int price[n_len] = {0,10,7,12,4,20};
     int dp[c_len][n_len] = {0};
     int trace[c_len][n_len] = {0};
-    int select[c_len] = {0};
+    int select[n_len] = {0};
     int w = 0, p = 0;   // w 存储第j个物品的重量，p 存储第j个物品的价格
+
     // 动态规划主体：遍历得dp数组与trace数组
     for (int i = 1; i < c_len; i++) {
         for (int j = 1; j < n_len; j++) {
@@ -40,28 +47,26 @@ int main() {
                 dp[i][j] = dp[i][j-1];
                 trace[i][j] = 0;
             } else {
-                if (dp[i-w][j-1] + p > dp[i][j-1]) {
-                    dp[i][j] = dp[i-w][j-1] + p;
-                    trace[i][j] = 1;
-                } else {
-                    dp[i][j] = dp[i][j-1];
-                    trace[i][j] = 0;
-                }
+                dp[i][j] = (dp[i-w][j-1]+p > dp[i][j-1]) ? dp[i-w][j-1]+p : dp[i][j-1];
+                trace[i][j] = (dp[i-w][j-1]+p > dp[i][j-1]) ? 1 : 0;
             }
         }
     }
+
+    // 打印dp数组 与 trace数组
     printf("dp:\n");
     Output2Darray(dp, c_len, n_len);
     printf("trace:\n");
     Output2Darray(trace, c_len, n_len);
-    // traceback
+
+    // traceback and printout
     for (int i = capacity, j = num; j > 0; j--) {
         if (trace[i][j] == 1) {
             select[j] = 1;
             i -= weight[j];
         }
     }
-    printf("\n");
+    printf("optimal things: \n");
     for (int i = 1; i < n_len; i++) {
         if (select[i] == 1) {
             printf("A%d ", i);
