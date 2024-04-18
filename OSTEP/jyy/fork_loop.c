f5148e9dfb82191863c5e4a48f4067fad9fa060d
hk416hasu <220221090331@ncepu.edu.cn>
2024-04-18 23:05:50 +0800

    update fork_loop.c

--- OSTEP/jyy/fork_loop.c
+++ OSTEP/jyy/fork_loop.c
@@ -2,9 +2,10 @@
 #include <unistd.h>
 
 int main() {
+    int grandpa = getpid();
     for (int i = 0; i < 2; i++) {
-        fork();
-        printf("Hello, %d\n", getpid());
+        fork(); // fork() will copy IR, so the loop_i will not be 0 always. The prograss and its children will continue form their IR(inherited), not from the beginning.
+        printf("Hello, %d (parent: %d)\n", getpid()-grandpa, getppid()-grandpa);
     }
     return 0;
 }
