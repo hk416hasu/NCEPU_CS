a6370852f041768a9d470805900173fc64d6edd9
hk416hasu <vbcgfh487882183@126.com>
2024-05-01 12:34:18 +0800

    solve upper limit

--- 2024/04/04/OptionalCourse/4solution_force.cc
+++ 2024/04/04/OptionalCourse/4solution_force.cc
@@ -21,6 +21,16 @@ void Print(std::vector<int> &vec) {
     printf("\n");
 }
 
+// 全为true返回true
+bool isAllTrue(std::vector<bool> vec) {
+    for (bool elem : vec) {
+        if (!elem) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
     int n = 0, target = 0, count = 0;
     scanf("%d %d", &n, &target);
@@ -29,12 +39,13 @@ int main() {
     std::vector<int> data(n);
     std::iota(data.begin(), data.end(), 1);
 
-    // 暴力枚举所有情况求符合题意的解，时间复杂度 O(2^n)
-    int ceiling = (int)pow(2, n);
-    std::vector<bool> access(n);    // ceiling 可能溢出
-    std::vector<int> sub;
-    for (int i = 1; i < ceiling; i++) {
-        Access_Init(access, i, n);
+    // 暴力枚举所有情况，时间复杂度 O(2^n)
+    std::vector<bool> access(n);
+    std::vector<int> sub;   // 保存中间结果，以便打印
+    int i = 0;
+    // 当且仅当access数组全为true时终止
+    while (!isAllTrue(access)) {
+        Access_Init(access, i++, n);
         for (int j = 0; j < n; j++) {
             if (access[j]) {
                 sub.push_back(data[j]);
