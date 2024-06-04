c872be746b1b1167a9ca7bcca8f931e912c226a5
hk416hasu <vbcgfh487882183@126.com>
2024-06-04 19:01:58 +0800

    abab

--- 2024/06/01/review/backtracking/MulElemsSubsets copy.cc
+++ 2024/06/01/review/backtracking/MulElemsSubsets copy.cc
@@ -1,4 +1,5 @@
 // 尝试使用打包重复元素的思路 解决重复元素子集问题, 但失败了
+// 这个版本可以输出所有子集
 
 #include <iostream>
 #include <vector>
@@ -8,7 +9,6 @@ using std::vector;
 
 class foo {
 public:
-    bool m_enable = false;
     char m_ch;  // 只能是数字字符 48<=ch<=57, 后续还可以加字母字符
     size_t m_times;
 
@@ -20,20 +20,6 @@ public:
     ~foo() {}
     // =重载? new重载?
 };
-
-// class foo {
-// public:
-//     char m_ch;  // 只能是数字字符 48<=ch<=57, 后续还可以加字母字符
-//     size_t m_times;
-
-//     foo() { m_ch = 0; m_times = 0; }
-//     foo(char ch, size_t times) {
-//         m_ch = ch;
-//         m_times = times;
-//     }
-//     ~foo() {}
-//     // =重载? new重载?
-// };
 
 bool input(vector<char> &str) {
     size_t n = 0;
@@ -66,7 +52,6 @@ bool GetFrequency(const vector<char> &str, vector<foo> &Frequency) {
     // step2: 将各数字字符及其出现频次打包成类数组
     for (size_t i = 0; i < 10; i++) {
         Frequency[i] = foo(i+48, bucket[i]);
-        if (bucket[i] > 0) { Frequency[i].m_enable = true; }
     }
 
     return true;
@@ -76,8 +61,8 @@ vector<char> result;
 int count = 0;
 
 void updateIndex(int &index, const vector<foo> &Frequency) {
-    for (int i = index; i < Frequency.size(); i++) {
-        if (Frequency[i].m_enable && Frequency[i].m_times > 0) {
+    for (int i = 0; i < Frequency.size(); i++) {
+        if (Frequency[i].m_times > 0) {
             index = i;
             return;
         }
@@ -93,13 +78,11 @@ void func(int index, vector<foo> &Frequency) {
         output(result);
     } else {
         result.push_back(Frequency[index].m_ch);
-        Frequency[index].m_enable = true;
         Frequency[index].m_times -= 1;
         
         func(index, Frequency);
         
         result.pop_back();
-        Frequency[index].m_enable = false;
         func(index, Frequency);
         Frequency[index].m_times += 1;
     }
