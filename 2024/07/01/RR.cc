e6c2e474771d6b46dd748be5d0a8a62f278fa4a9
hk416hasu <vbcgfh487882183@126.com>
2024-07-04 19:30:20 +0800

    easy job

---
+++ 2024/07/01/RR.cc
@@ -0,0 +1,94 @@
+#include <cstdio>
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+#include <cassert>
+
+using std::vector;
+
+#define ProcessNum 5
+
+enum ProcessState { R, E };
+
+class Process {
+public:
+    int m_id;
+    int m_next;
+    int m_NeedServeTime;
+    int m_ServedTime;
+    ProcessState m_stat;
+
+    Process(int id, int NeedServeTime) : m_id(id), m_ServedTime(0), m_stat(R) {
+        m_next = (id + 1) % ProcessNum;
+        m_NeedServeTime = NeedServeTime;
+    }
+
+    void Print() {
+        printf("Process %d:\n\tnext -> %d,\n\tNeedServeTime: %d,\n\tServedTime: %d,\n\tstate: %d\n", \
+            m_id, m_next, m_NeedServeTime, m_ServedTime, m_stat);
+    }
+
+    void Execute() {
+        m_ServedTime++;
+        if (m_ServedTime == m_NeedServeTime) {
+            m_stat = E;
+        }
+    }
+
+};
+
+void CreateProcessArray(vector<Process> &ProcessArray, int num) {
+    assert(num >= 1);
+    std::srand(std::time(0) + std::rand()); // 设置随机数种子
+    for (int i = 0; i < num; i++) {
+        ProcessArray.emplace_back( i, ((std::rand() % 5) + 1) );
+    }
+}
+
+// 若所有进程均End, 则返回true, 否则返回false
+bool isAllEnd(const vector<Process> &PArr) {
+    for (Process elem : PArr) {
+        if (elem.m_stat == R) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void Print(const vector<Process> &PArr) {
+    for (Process elem : PArr) {
+        elem.Print();
+    }
+}
+
+void RR(vector<Process> &PArr) {
+
+    int RP = 0; // RunningPointer
+
+    while (!isAllEnd(PArr)) {   // RR结束条件
+        printf("\n\n\n");
+        Print(PArr);    // 输出状态
+
+        if (PArr[RP].m_stat == R) {
+            PArr[RP].Execute();
+        }
+
+        RP = PArr[RP].m_next;
+
+    }
+
+}
+
+int main() {
+
+    assert(ProcessNum > 0);
+    
+    vector<Process> ProcessArray;
+    CreateProcessArray(ProcessArray, ProcessNum);
+
+    printf("enum ProcessState { R, E }");
+    RR(ProcessArray);
+
+    return 0;
+}
