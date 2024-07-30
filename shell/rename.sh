9f3038648a30093d62a3d312bf2278ade0a35c78
hk416hasu <487882183@qq.com>
2024-07-30 14:03:43 +0800

    update rename.sh

--- shell/rename.sh
+++ shell/rename.sh
@@ -3,10 +3,12 @@
 cd ~/Desktop/the_missing_semester/Chinese
 
 for file in *; do
-    echo $file | awk '{print $4 $5 $6}'
-    #$newname=$(echo $file | awk '{print $4 $5 $6}')
-    #echo $newname
-    # mv $file $newname
+    # echo $file | awk '{print $4 $5 $6}'
+    newname=$(echo $file | awk '{print $4 " " $5 " " $6}')
+    # echo $newname
+    mv "$file" "$newname"
+    # mv $file "$newname"  is not okay, because file contains space so that it will be interrupted to multiple arguments.
+
 done
 
 exit 1
