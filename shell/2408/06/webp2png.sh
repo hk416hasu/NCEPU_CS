87a50b2204e461b26a2142dc89e1d4970a4d7837
hk416hasu <487882183@qq.com>
2024-08-07 01:15:32 +0800

    update

--- shell/2408/06/webp2png.sh
+++ shell/2408/06/webp2png.sh
@@ -1,8 +1,9 @@
 #!/bin/env bash
 
-for file in *.webp; do
-    convert "$file" "${file%.webp}.png"
-    rm "$file"
+for file in **/*.webp; do
+    if [[ -f "$file" ]]; then
+        convert "$file" "${file%.webp}.png" && rm "$file"
+    fi
 done;
 
 exit 0
