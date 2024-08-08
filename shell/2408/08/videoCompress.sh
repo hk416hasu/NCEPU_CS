45c846efb33c553ae770a5a9226bdbd95acf660b
hk416hasu <vbcgfh487882183@126.com>
2024-08-08 19:07:39 +0800

    little modification

--- shell/2408/08/videoCompress.sh
+++ shell/2408/08/videoCompress.sh
@@ -3,8 +3,8 @@
 shopt -s globstar
 
 # use varible to replace static path
-echo "plz give the absolute path of target dir"
-read -r tardir     # get  target directory from input 
+echo "plz give the absolute path(from /) of target dir"
+read -r tardir     # get target directory from input 
 if [[ ! -d "$tardir" ]]; then
     echo "$tardir" "doesn't exist."
     exit 1
@@ -24,7 +24,7 @@ fi
 cd "$(dirname "$tardir")" || exit 1
 relapath='./'$(basename "$tardir")
 name=$(basename "$tardir")
-newname="[640x360]""$name"
+newname="[abab]""$name"
 
 for file in "$relapath"/**/*.{jpg,png}; do
     newpath="${file/"$name"/"$newname"}"
@@ -35,8 +35,8 @@ done
 
 for file in "$relapath"/**/*.mp4; do
     newpath="${file/"$name"/"$newname"}"
-#    newdir="$(dirname "$newpath")"
-#    mkdir -p "$newdir"
+    newdir="$(dirname "$newpath")"
+    mkdir -p "$newdir"
     ffmpeg -i "$file" -vf scale=640:-1 "$newpath"
 done
 
