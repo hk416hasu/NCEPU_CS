fca078197e2c40379973bdb279d7e3b69e4fe6a8
hk416hasu <487882183@qq.com>
2024-08-08 11:58:33 +0800

    add shopt -s globstar

--- shell/2408/06/webp2png.sh
+++ shell/2408/06/webp2png.sh
@@ -1,5 +1,7 @@
 #!/bin/env bash
 
+shopt -s globstar
+
 for file in **/*.webp; do   # ** will Lists complete file tree, recursively.
     if [[ -f "$file" ]]; then
         convert "$file" "${file%.webp}.png" && rm "$file"
