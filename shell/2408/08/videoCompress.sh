dd9d74d0492cb432df8f17b9de28eacb43cf9144
hk416hasu <487882183@qq.com>
2024-08-08 14:24:52 +0800

    videoCompress v2

--- shell/2408/08/videoCompress.sh
+++ shell/2408/08/videoCompress.sh
@@ -8,3 +8,12 @@ for file in ./anime/**/*.{jpg,png}; do
     mkdir -p "$newdir"
     cp "$file" "$newpath"
 done
+
+for file in ./anime/**/*.mp4; do
+    newpath="${file/anime/anime_640x360}"
+#    newdir="${newpath%/*}"
+#    mkdir -p "$newdir"
+    ffmpeg -i "$file" -vf scale=640:-1 "$newpath"
+done
+
+exit 0
