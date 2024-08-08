c8fb537fe0d42340014ea8ab427ffc1af4a2f034
hk416hasu <487882183@qq.com>
2024-08-08 14:23:54 +0800

    videocompress v1

---
+++ shell/2408/08/videoCompress.sh
@@ -0,0 +1,10 @@
+#!/bin/env bash
+
+shopt -s globstar
+
+for file in ./anime/**/*.{jpg,png}; do
+    newpath="${file/anime/anime_640x360}"
+    newdir="${newpath%/*}"
+    mkdir -p "$newdir"
+    cp "$file" "$newpath"
+done
