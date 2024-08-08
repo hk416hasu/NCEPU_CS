365a1de280134929d27f5f2360118be2b7d48210
hk416hasu <vbcgfh487882183@126.com>
2024-08-08 18:16:24 +0800

    upgrade

--- shell/2408/08/videoCompress.sh
+++ shell/2408/08/videoCompress.sh
@@ -5,7 +5,6 @@ shopt -s globstar
 # use varible to replace static path
 echo "plz give the absolute path of target dir"
 read -r tardir     # get  target directory from input 
-# plz trans ~ later
 if [[ ! -d "$tardir" ]]; then
     echo "$tardir" "doesn't exist."
     exit 1
@@ -16,48 +15,29 @@ ls "$tardir" | head -n 5
 open "$tardir"
 echo -e "\nis this your target dir?"
 echo -n "enter yes/no: "
-read flag
+read -r flag
 
 if [[ "$flag" != "yes" ]]; then
     exit 2
 fi
 
-cd "$(dirname "$tardir")"
-# pwd
+cd "$(dirname "$tardir")" || exit 1
 relapath='./'$(basename "$tardir")
-# echo $relapath
-oldname=$(basename "$tardir")
-newname="[640x360]""$oldname"
+name=$(basename "$tardir")
+newname="[640x360]""$name"
 
-for file in $relapath/**/*.{jpg,png}; do
-    newpath="${file/"$oldname"/"$newname"}"
+for file in "$relapath"/**/*.{jpg,png}; do
+    newpath="${file/"$name"/"$newname"}"
     newdir="$(dirname "$newpath")"
     mkdir -p "$newdir"
     cp "$file" "$newpath"
 done
 
-for file in $relapath/**/*.mp4; do
-    newpath="${file/"$oldname"/"$newname"}"
+for file in "$relapath"/**/*.mp4; do
+    newpath="${file/"$name"/"$newname"}"
 #    newdir="$(dirname "$newpath")"
 #    mkdir -p "$newdir"
     ffmpeg -i "$file" -vf scale=640:-1 "$newpath"
 done
-
-
-: '    
-for file in ./anime/**/*.{jpg,png}; do
-    newpath="${file/anime/anime_640x360}"
-    newdir="${newpath%/*}"
-    mkdir -p "$newdir"
-    cp "$file" "$newpath"
-done
-
-for file in ./anime/**/*.mp4; do
-    newpath="${file/anime/anime_640x360}"
-#    newdir="${newpath%/*}"
-#    mkdir -p "$newdir"
-    ffmpeg -i "$file" -vf scale=640:-1 "$newpath"
-done
-'
 
 exit 0
