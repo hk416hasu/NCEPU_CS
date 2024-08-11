6129ab9d256212ca97c3a9932ca3166ae8b99af9
hk416hasu <vbcgfh487882183@126.com>
2024-08-11 15:43:05 +0800

    upgrade nohup

--- shell/2408/11/60fps.sh
+++ shell/2408/11/60fps.sh
@@ -2,7 +2,8 @@
 
 for file in *.mp4; do
     output="[60fps]$file"
-    ffmpeg -i "$file" -vf "minterpolate='fps=60'" "$output"
+    nohup ffmpeg -i "$file" -vf "minterpolate='fps=60'" "$output" > /dev/null 2>&1 &
+    # redirect stdout to /dev/null, and redirect stderr to stdout
 done
 
 exit 0
