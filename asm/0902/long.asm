35d9eaa672606a98ebb934de7ec88f58364ac18c
hk416hasu <vbcgfh487882183@126.com>
2024-09-02 23:05:57 +0800

    still learning asm

--- asm/0902/long.asm
+++ asm/0902/long.asm
@@ -1,18 +1,34 @@
 ; input 3 digits (in decimal)
+data segment
+    base dw 10h    
+data ends
+
 code segment
-    assume cs:code
+    assume cs:code, ds:data
 start:
 
-; input the hundreds digit
+; Load DS with the address of the data segment
+mov ax, data
+mov ds, ax
+
+; input the HUNDREDS DIGIT
 mov ah, 1h
 int 21h     ; now the ASCII is in AL
 sub al, 30h ; get the value
-mov bl, al
 
 ; mul with base
-mov al, 10h
+    ; attention ! mul R will mul R with AL/AX, and the res will in AX(DX)
+mov bl, base
 mul bl
+mov bx, ax
 
+; input the TENS DIGIT
+mov al, 1h
+int 21h
+sub al, 30h
+
+; mul with base
+mov 
 
 
 ; return control to the terminal
