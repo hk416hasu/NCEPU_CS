c88777c1cc3c90469d14268af0192fb4f39080ea
hk416hasu <vbcgfh487882183@126.com>
2024-09-05 12:12:50 +0800

    add a script to combine png to pdf with correct order

---
+++ shell/2409/05/convert.sh
@@ -0,0 +1,18 @@
+#!/bin/env bash
+
+# At first, rename here
+for file in *.png; do 
+    name=${file%.*}
+    if [[ ${#name} -eq 1 ]]; then
+        mv "$file" "00$file"
+    elif [[ ${#name} -eq 2 ]]; then
+        mv "$file" "0$file"
+    fi
+done
+
+# Second, convert them to pdf
+convert *.png "../$(basename $PWD).pdf"
+
+# then u can use pdf-viewer to print that pdf to Four Pages One Side
+
+exit 0
