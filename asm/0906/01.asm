3111ea35ef113de107b4a120a61a5e6f815427e4
hk416hasu <vbcgfh487882183@126.com>
2024-09-06 11:17:42 +0800

    modify the template.asm

---
+++ asm/0906/01.asm
@@ -0,0 +1,14 @@
+; output a num in any base
+
+code segment
+    assume cs:code
+start:
+
+mov 
+
+
+mov ah, 4ch
+int 21h
+
+code ends
+    end start
--- asm/template.asm
+++ asm/template.asm
@@ -1,24 +1,30 @@
-;data segment
-    ; some data
-;data ends
+; Template for 8086 Assembly Program
 
-;stack segment
-    ; stack 
-;stack ends
+data segment
+    msg db 'hello world!!$', 0 ; Define a customizable message string
+data ends
+
+stack segment stack
+    dw 0,0,0,0,0,0,0,0
+stack ends
 
 code segment
-    assume cs:code ;, ds:data, ss:stacks
+    assume cs:code, ds:data, ss:stack
+
 start:
+    ; Set up the data segment
+    mov ax, data
+    mov ds, ax
 
+    ; Display the message
+    lea dx, msg
+    mov ah, 9
+    int 21h
 
-
-    ; codes
-
-
-
-    ; return control to the terminal
+    ; Return control to DOS
     mov ah, 4ch
     int 21h
 
 code ends
     end start
+
