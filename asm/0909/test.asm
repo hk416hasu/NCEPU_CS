779cae9e13a4eb49000246db19b4907a15e3b1c9
hk416hasu <vbcgfh487882183@126.com>
2024-09-10 16:48:51 +0800

    test bias of data

---
+++ asm/0909/test.asm
@@ -0,0 +1,32 @@
+; Template for 8086 Assembly Program
+
+data segment
+    msg db 'hello world!!$', 0 ; Define a customizable message string
+data ends
+
+stack segment stack
+    dw 0,0,0,0,0,0,0,0
+stack ends
+
+code segment
+    assume cs:code, ds:data, ss:stack   ; put into segment registers
+
+start:
+    ; Set up the data segment
+    mov ax, data    ; because INTEL tells us to use segment register
+    mov ds, ax
+;    mov ds, data:[0000]
+;    mov ax, [0001]
+
+    ; Display the message
+    lea dx, msg
+    mov ah, 9
+    int 21h         ; system calls, plz RTFM(about dos) for more
+
+    ; Return control to DOS
+    mov ah, 4ch
+    int 21h
+
+code ends
+    end start
+
