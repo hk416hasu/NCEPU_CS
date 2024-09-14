4f3479fb8ade0a3fe20cb88c2c12c6cc999b6678
hk416hasu <vbcgfh487882183@126.com>
2024-09-14 12:02:55 +0800

    fixed the timer_init() bug

--- 8051/09/13/timer0.c
+++ 8051/09/13/timer0.c
@@ -3,8 +3,8 @@
 void Timer0_Init() {
     TMOD &= 0xf0;   // clear Timer0 configure
     TMOD |= 0x01;   // set timer0 to 16-bit mode
-    TH0 = (65536 - 60000) >> 8;  // set the High 8 bits of timer0
-    TL0 = (65536 - 60000) & 0xff; // set the Low 8 bits of timer0
+    TH0 = (65536 - 100) >> 8;  // set the High 8 bits of timer0
+    TL0 = (65536 - 100) & 0xff; // set the Low 8 bits of timer0
     ET0 = 1;    // enable interrupt of timer0
     EA = 1;     // enable global interrupts
     TR0 = 1;    // enable timer0
