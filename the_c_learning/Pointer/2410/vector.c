aecdd1d83cd6eb73ebeec960613ebb323de08330
hk416hasu <vbcgfh487882183@126.com>
2024-10-02 16:46:01 +0800

    learn pointer to array

--- the_c_learning/Pointer/2410/vector.c
+++ the_c_learning/Pointer/2410/vector.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main() {
+void func1() {
     int vector[] = {1, 2, 3};
     int* p = vector;    // vector contains the addr of the array's first elem,
                         // and p is assigned to vector, so they are equal
@@ -12,6 +12,39 @@ int main() {
 
     p++;
     printf("%d\n", *p); // output vector[1]
+
+}
+
+void func2() {
+    int vec[] = {2, 3, 4, 5, 6};
+    int *p = vec;
+
+    printf("%p\n", p);
+    printf("%ld\n", sizeof(p));
+    printf("%p\n", vec);
+    printf("%ld\n", sizeof(vec));
+    printf("%p\n", &vec[0]);
+    printf("%ld\n", sizeof(&vec[0]));
+    printf("%p\n", &vec);
+    printf("%ld\n", sizeof(&vec));
+}
+
+void func3_sub(int (*foo)[5]) { // u need to give the length, so it's useless..
+//    printf("%ld\n", sizeof(*foo)/sizeof((*foo)[0]));
+//    printf("%d, %d\n", (*foo)[0], (*foo)[1]);
+}
+
+void func3() {
+    int vec[] = {1, 2, 3, 4, 5};
+//    printf("%ld\n", sizeof(vec)/sizeof(vec[0]));
+    int (*p)[5] = &vec;
+    printf("%ld\n", sizeof(*p)/sizeof((*p)[0]));
+    func3_sub(p);
+//    printf("%d\n", (*p)[3]);
+}
+
+int main() {
+    func3();
 
     return 0;
 }
