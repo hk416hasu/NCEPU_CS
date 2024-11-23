566c8380de109561158ba1979f4642802a62859d
hk416hasu <hk416hasu@126.com>
2024-11-23 13:03:13 +0800

    add more test func in scanf()

--- the_c_learning/king/2411/scanf.c
+++ the_c_learning/king/2411/scanf.c
@@ -3,20 +3,72 @@
 void test_1();
 void test_2();
 void test_3();
+void test_4();
+void test_5();
 
 int main() {
 
-    test_3();
 
     return 0;
+}
+
+/*
+   * brief : check the Assignment-Suppression *
+   * input : 1 2
+   * output: 2
+   * conclusion: the * tells scanf() to read the input but not store it in a variable
+   */
+void test_6() {
+    int num = 0;
+    scanf("%*d          %d", &num);
+    printf("%d\n", num);
+}
+
+/*
+   * brief : check the working way of scanf()
+   * conclusion: scanf will ignore the previous multi-space,
+        and it work as a "string matching func", our conversion-
+        specifier tells it how to match input
+   */
+void test_5() {
+    int arr[2] = {0};
+
+    arr[0] = 0; arr[1] = 0;
+    scanf("%d%d", &arr[0], &arr[1]);
+    printf("%d, %d\n", arr[0], arr[1]);
+
+    arr[0] = 0; arr[1] = 0;
+    scanf("%d %d", &arr[0], &arr[1]);
+    printf("%d, %d\n", arr[0], arr[1]);
+
+    arr[0] = 0; arr[1] = 0;
+    scanf("%d,%d", &arr[0], &arr[1]);
+    printf("%d, %d\n", arr[0], arr[1]);
+}
+
+/*
+   * brief : check the exceed behaviour (%d -> short, is wrong)
+   * input : 32768
+   * output: 
+        -32768
+        *** stack smashing detected ***: terminated
+        Aborted (core dumped)
+   * conclusion: it will be terminated, but we should focus on the boarder
+   */
+void test_4() {
+    short num = 0;
+    scanf("%hd", &num); // gcc tells me to use the match conversion specifier with a "type modifier"
+    // similarily, %ld -> long integer, %hd -> short integer
+    // scanf("%d", &num); // this will exceed, and gcc will warn
+    printf("%d\n", num);
 }
 
 /*
    * brief : test %width and sscanf()
    * input : NULL(don't need input)
    * output: 5 14 893 0 0
-   * conclusion: the width limit the "upper" of input, and when no input, 
-   *                it will assign 0
+   * conclusion: the "width" limit the "upper" of input, 
+                    and when no input, it will assign 0
    */
 void test_2() {
     char str[10] = "514893";
