dc41b6c4d7a8af272d97acd240d47f0e90e54f74
hk416hasu <hk416hasu@126.com>
2025-01-03 09:42:47 +0800

    package the isbn.js and isbnSearch.cpp

---
+++ 2025/01/03/isbnSearch.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+void isbn_search(const char *isbn) {
+    char command[100];
+    snprintf(command, sizeof(command), "nodejs ./isbn.js %s > ./info.json", isbn);
+    assert(system(command) != -1);
+}
+
+int main(void) {
+    const char isbn[] = "9787115539168";
+    isbn_search(isbn);
+    return 0;
+}
+
---
+++ front-end/javascript/isbn.js
@@ -0,0 +1,62 @@
+/**
+ * npm install crypto-js node-fetch@2
+ */
+const CryptoJS = require("crypto-js");
+const querystring = require("querystring");
+const fetch = require("node-fetch");
+
+// 云市场分配的密钥Id
+const secretId = "AKIDuJ5UiSKdEk9bfS56cbXm8pQc1mnyXp4tuc";
+// 云市场分配的密钥Key
+const secretKey = "LEt8aBZSXHK7xax6F5mm3jKOn0xCRD5Y55xZSLtQ";
+const source = "market";
+
+// 签名
+const datetime = new Date().toGMTString();
+const signStr = "x-date: " + datetime + "\n" + "x-source: " + source;
+const sign = CryptoJS.enc.Base64.stringify(CryptoJS.HmacSHA1(signStr, secretKey));
+const auth = `hmac id="${secretId}", algorithm="hmac-sha1", headers="x-date x-source", signature="${sign}"`;
+
+// 请求方法
+const method = "GET";
+
+// 请求头
+const headers = {
+    "X-Source": source,
+    "X-Date": datetime,
+    "Authorization": auth,
+};
+
+// 查询参数
+const queryParams = {
+    "isbn": "9787115539168",
+};
+
+// 请求 URL 参数拼接
+let url = "https://service-osj3eufj-1255468759.ap-shanghai.apigateway.myqcloud.com/release/isbn";
+if (Object.keys(queryParams).length > 0) {
+    url += "?" + querystring.stringify(queryParams);
+}
+
+// 发送请求
+(async () => {
+    try {
+        const options = {
+            method,
+            headers,
+        };
+
+        const response = await fetch(url, options);
+        const body = await response.text();
+
+        if (!response.ok) {
+            console.error("Error:", response.status, response.statusText);
+            return;
+        }
+
+        console.log(body);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+})();
+
