a5d2b38fdb51dc6fd80b3ef90aa7880cc067d0a5
hk416hasu <hk416hasu@126.com>
2025-01-20 17:50:48 +0800

    learn .S and macro program, and AT&T asm

---
+++ 2025/01/20/manual/minimal.s
@@ -0,0 +1,21 @@
+# GCC use AT&T format of assembly
+
+.section .text
+.globl _start # global identifier
+_start:
+  mov $1, %rax
+  mov $1, %rdi
+  # mov $message, %rsi
+  lea message, %rsi
+  # mov $6, %rdx
+  mov $(message_end - message), %rdx
+  syscall
+
+  mov $60, %rax; mov $0, %rdi; syscall
+
+.section .data
+message: .ascii "\033[01;05;31mHello World!\033[0m\n"
+message_end:
+
+/* vim: ft=gas :
+*/
